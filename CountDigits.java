d419d70c9f8171c01844d7dedb53a774be63dd1a
smohan73866 <131269867+smohan73866@users.noreply.github.com>
2023-06-13 16:20:31 +0530

    Add files via upload

---
+++ CountDigits.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class CountDigits{
+public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);
+         String str=sc.nextLine();
+         int count=0;
+         for(int i=0;i<=str.length()-1;i++){
+if((str.charAt(i)=='a')||(str.charAt(i)=='e')||
+(str.charAt(i)=='i')||(str.charAt(i)=='o')||
+(str.charAt(i)=='u')||(str.charAt(i)=='A')||
+(str.charAt(i)=='e')||(str.charAt(i)=='I')||
+(str.charAt(i)=='o')||(str.charAt(i)=='U')){
+                  count++;
+              }
+}
+System.out.println(count);
+}
+}
+
+
---
+++ StrFormat.java
@@ -0,0 +1,11 @@
+public class StrFormat
+{
+/*Driver Code*/
+public static void main(String args[])
+{
+ String s1=new String("Hello");
+ String s2=new String("World");
+String s=String.format("%s%s",s1,s2);
+System.out.println(s.toString());
+}
+}
