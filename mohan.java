08d0eaeecbbe1639728659b855415b8da71dab24
smohan73866 <131269867+smohan73866@users.noreply.github.com>
2023-06-13 16:17:54 +0530

    Add files via upload

---
+++ mohan.java
@@ -0,0 +1,17 @@
+class A{
+public void GOD(){
+System.out.print("GOD called from classA");
+}
+public void GHOST(){
+System.out.print("GHOSTcalled from classA");
+}
+
+}
+class B
+{
+public static void main(String args[]){
+A ob=new A();
+ob.GOD();
+ob.GHOST();
+}}
+ 
---
+++ tharun.java
@@ -0,0 +1,14 @@
+class B
+{
+public static void main(){
+System.out.println("Main method without argument called.");
+}
+public static void main (string args[]){
+system.out.println("Anil is typing");
+main();
+main(20);
+}
+public static void main(int x){
+System.out.println("Main method with argument called"+x*2);
+}
+}
